3ae16ec3d9d1086d8bc33dabef1df7daf4788447
Giorgio Zett <giorgio.zett@interlogica.it>
2019-09-08 10:06:32 +0200

    improve browser and device position check

--- src/vrmode/index.mjs
+++ src/vrmode/index.mjs
@@ -1,17 +1,17 @@
 import broserCheckService from './service/BrowserCheckService.js'
+import reportService from './service/ReportService.js'
 import CONST from './const.js';
 import './components/index.js';
 
 function startup() {
-    // try to fix device orientation and fullscreen mode
-    if (screen.orientation.lock) {
-        screen.orientation.lock('landscape')
-            .catch(err => console.log(err));
+    if(CONST.test && CONST.test.enabled === true) {
+        const tdata = CONST.test;
+        localStorage.setItem("expires_at", tdata.expires_at);
+        localStorage.setItem("id_token", tdata.id_token);
+        localStorage.setItem("access_token", tdata.access_token);
     }
-
     // check browser
     if (broserCheckService.isARAvailable()) {
-        
         Promise.all(
                 [
                     import('./service/AuthService.js').then(v => v.default),
@@ -23,6 +23,10 @@ function startup() {
                 authService.auth()
                     .then(socketService.connect.bind(socketService))
                     .then(() => {
+                        if (screen.orientation.lock) {
+                            screen.orientation.lock('landscape')
+                                .catch(err => console.log(err));
+                        }
                         return Promise.all([
                             import('./service/RegisterComponentService.js').then(v => v.default),
                             import('./service/PageService.js').then(v => v.default)
@@ -30,32 +34,39 @@ function startup() {
                     })
                     .then(([registerComponentService, pageService]) => {
                         setTimeout(() => {
-
                             const tmpl = document.querySelector('#tmpl-scene');
                             document.body.appendChild(document.importNode(tmpl.content, true));
-                            const p = document.querySelector('.poster');
+                            
                             const s = document.querySelector('a-scene');
                             s.classList.remove("hidden");
-                            p.parentNode.removeChild(p);
                             registerComponentService.register();
                             pageService.start();
+
+                            const p = document.querySelector('.poster');
+                            p.parentNode.removeChild(p);
+                            
                             document.dispatchEvent(new CustomEvent("ShowWaitRound"));
                         }, 2000)
                     })
                     .catch((err) => {
-                        console.error("Nothing works...WTF!!", err);
-                        //location.href = CONST.APP_FALLBACK;
+                        reportService.report(err.message)
+                        .then(() => {
+                            location.href = CONST.app_old;
+                        });
                     })
             })
             .catch((err) => {
-                console.error("Nothing works...WTF!!", err);
-                // location.href = CONST.APP_FALLBACK;
+                reportService.report(err.message)
+                .then(() => {
+                    location.href = CONST.app_old;
+                });
             })
     } else {
-        // location.href = CONST.APP_FALLBACK;
-        console.log('error');
+        reportService.report('unsupported browser')
+        .then(() => {
+            location.href = CONST.app_old;
+        });
     }
 }
-
 
 window.addEventListener("load", startup, false);
--- src/vrmode/service/BrowserCheckService.js
+++ src/vrmode/service/BrowserCheckService.js
@@ -3,13 +3,33 @@ export default {
     browsers: [
         'AppleWebKit/605',
         'Version/12.0',
+        'Mozilla',
         'Chrome'
     ],
+
+    ORIENTATION: {
+        LANDSCAPE : 'landscape',
+        PORTRAIT: 'portrait'
+    },
 
     isARAvailable() {
         const ua = navigator.userAgent;
         return this.browsers.some( function(item) {
             return ua.indexOf(item) > -1;
         })
+    },
+
+    getOrientation() {
+        if (window.screen.orientation) {
+            const expr = /landscape/;
+            return expr.test(window.screen.orientation.type) ? this.ORIENTATION.LANDSCAPE : this.ORIENTATION.PORTRAIT;
+        } else if(window.matchMedia("(orientation: portrait)").matches ) {
+            return this.ORIENTATION.PORTRAIT;
+            
+        } else if(window.matchMedia("(orientation: landscape)").matches ) {
+            return this.ORIENTATION.LANDSCAPE;
+        } else {
+            return (window.innerHeight > window.innerWidth) ? this.ORIENTATION.PORTRAIT : this.ORIENTATION.LANDSCAPE;
+        }
     }
 }
