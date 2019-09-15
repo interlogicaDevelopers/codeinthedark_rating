e99c2aeba90620ef2b7b01491e9a67d4fa8c4dae
Giorgio Zett <giorgio.zett@interlogica.it>
2019-09-15 18:21:36 +0200

    add rollup

---
+++ rollup.config.js
@@ -0,0 +1,39 @@
+import html from '@beyonk/rollup-plugin-html-esm'
+import cleaner from 'rollup-plugin-cleaner';
+import copy from 'rollup-plugin-copy'
+
+export default {
+    input: ["src/vrmode/index.mjs"],
+    output: [
+      // ES module version, for modern browsers
+      {
+        dir: "dist/vrmode",
+        format: "es",
+        sourcemap: false,
+        compact: true
+      }
+    ],
+    plugins: [
+        cleaner({
+            targets: [
+              'dist/vrmode/'
+            ]
+        }),
+        copy({
+            targets: [
+              { src: 'src/vrmode/assets/**/*', dest: 'dist/vrmode/assets' },
+              { src: 'src/vrmode/libs/**/*', dest: 'dist/vrmode/libs' },
+              { src: 'src/vrmode/index.css', dest: 'dist/vrmode' },
+              { src: 'src/vrmode/manifest.json', dest: 'dist/vrmode' },
+            ]
+        }),
+        html({
+          // specify template html (optional)
+          template: 'src/vrmode/index.tmpl.html',  // Default undefined
+          // output filename (optional)
+          filename: '../dist/vrmode/index.html', // Default index.html
+          // when specified, js src will use absolute path from publicPath (optional)
+          publicPath: 'src' // Default undefined
+        })
+      ]
+};
